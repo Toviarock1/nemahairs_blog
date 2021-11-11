cc6dc21d6903a777e96db5fd0036d889bff279a1
simon adama <simontoviaadama@gmail.com>
2021-11-11 12:56:40 +0100

    test useEffect hook to see if google analytics will work properly

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from '../../views/Footer/Footer';
 import Header from '../../views/Header/Header';
 import ReactGa from 'react-ga';
 
 const Layout = (props) => {
+    
+    useEffect(() => {
+        ReactGa.initialize('UA-212602997-1');
+        ReactGa.pageview(window.location.pathname + window.location.search);
+    })
 
-    ReactGa.initialize('UA-212602997-1');
-    ReactGa.pageview(window.location.pathname + window.location.search);
-
+   
     return (
         <>
             <Header searchText={props.searchText} setSearchText={props.setSearchText} />
