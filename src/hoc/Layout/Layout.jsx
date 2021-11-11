3ef072fe0ed54058c0877fbfb20fa371498731cc
simon adama <simontoviaadama@gmail.com>
2021-11-11 01:51:49 +0100

    add google analytics

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Footer from '../../views/Footer/Footer';
 import Header from '../../views/Header/Header';
+import ReactGa from 'react-ga';
 
 const Layout = (props) => {
+
+    ReactGa.initialize('UA-212602997-1');
+    ReactGa.pageview(window.location.pathname + window.location.search);
+
     return (
         <>
             <Header searchText={props.searchText} setSearchText={props.setSearchText} />
