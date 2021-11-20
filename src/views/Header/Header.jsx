8ab6d7fc3d67fad88861953f52d985060e6b3fa6
simon adama <simontoviaadama@gmail.com>
2021-11-20 19:51:07 +0100

    fix header links width

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -10,21 +10,21 @@ const Header = (props) => {
         <header>
             <Navbar collapseOnSelect className={classes.Nav} expand="lg" bg="dark" variant="dark">
                 <Container fluid className={classes.Container}>
-                    <Navbar.Brand href="/">Nemahairs</Navbar.Brand>
+                    <Navbar.Brand href="/" className={classes.Logo}>Nemahairs</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="m-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/about">About</Nav.Link>
-                            <Nav.Link href="/advertise">Advertise</Nav.Link>
-                            <NavDropdown title="All Post" id="collasible-nav-dropdown">
+                            <Nav.Link href="/" className={classes.Link}>Home</Nav.Link>
+                            <Nav.Link href="/about" className={classes.Link}>About</Nav.Link>
+                            <Nav.Link href="/advertise" className={classes.Link}>Advertise</Nav.Link>
+                            <NavDropdown title="All Post" id="collasible-nav-dropdown" className={classes.Link}>
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="/contact">Contact</Nav.Link>
+                            <Nav.Link href="/contact" className={classes.Link}>Contact</Nav.Link>
                         </Nav>
                         <Nav>
                             <HeaderForm searchText={props.searchText} setSearchText={props.setSearchText} />
